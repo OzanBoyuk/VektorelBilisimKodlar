24119f2c5c038b62957fa0b46985ef7516e56c2e
vektorel <vektorel@lab8-8>
2017-08-09 20:22:18 +0300

    09.08.2017 Çarşamba Proje Hakkında Bilgi

---
+++ Ders19/src/Test.java
@@ -0,0 +1,31 @@
+
+public class Test {
+
+	/*Restauran projesi kullanýlacak teknolojiler
+	 * 1- Web Teknoloji 
+	 * 2- Rest Servis
+	 * 3- Mobil
+	 * 4- JSF, Primefaces, Android, Spring Framework, Dozer
+	 * 
+	 * 
+	 * Proje Gereksinimleri
+	 * 1- Kullanýcý kayýtlarý
+	 * 2- Müþteri formu
+	 * 3- Sipariþ Formu
+	 * 4- Menu Formu
+	 * 5- malzeme Formu
+	 * 6- Demirbaþ Formu
+	 * 7- Ana Ekran
+	 * 
+	 * Sýnýflar ve deðiþkenleri
+	 * - Basic model : id, CREATE_TIME, UPDATE_TIME, CREATE_USER_ID, UPDATE_USER_ID,DELETABLE
+	 * - DAO : insert, delete, getList ...
+	 * - 
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
